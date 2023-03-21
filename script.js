caf20f3078265baaf3fa94bb38f9e84dbbb92bfd
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-03-21 11:54:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function letterCombinations(input_digit) {
   //Complete the function
 
-
+if (input_digit.length == 0) {
+    var arr = [];
+    arr.push("");
+    return arr;
+  }
+  let fd = input_digit[0];
+  let ans = [];
+  let ros = input_digit.substring(1);
+  let str = obj11[fd];
+  for (let i = 0; i < str.length; i++) {
+    let fc = str[i];
+    let smallans = letterCombinations(ros);
+    for (var val of smallans) {
+      let smans = fc + val;
+      ans.push(smans);
+    }
+  }
+  return ans;
 }
 
 module.exports = letterCombinations;
